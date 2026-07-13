d64bfbb696fb22c05f9e7166a7f77cbf8f19ef4c
krispyking24 <77263183+krispyking24@users.noreply.github.com>
2026-07-13 13:28:24 +1000

    remove black list
    Removed various properties from the PROPERTIES_BLACKLIST.

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/util/WrenchMenuUtil.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/util/WrenchMenuUtil.java
@@ -7,37 +7,5 @@ import java.util.List;
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.*;
 public class WrenchMenuUtil {
 	public static final List<Property<?>> PROPERTIES_BLACKLIST = List.of(
-			EYE,
-			DISTANCE,
-			SNOWY,
-			HAS_BOOK,
-			HAS_RECORD,
-			LEVEL_CAULDRON,
-			LEVEL_COMPOSTER,
-			LEVEL_FLOWING,
-			LEVEL_HONEY,
-			LEVEL,
-			LAYERS,
-			BITES,
-			CANDLES,
-			EGGS,
-			STAGE,
-			PICKLES,
-			MOISTURE,
-			FLOWER_AMOUNT,
-			BED_PART,
-			AGE_1,
-			AGE_2,
-			AGE_3,
-			AGE_4,
-			AGE_5,
-			AGE_7,
-			AGE_15,
-			AGE_25,
-			NOTEBLOCK_INSTRUMENT,
-			SIGNAL_FIRE,
-			SLAB_TYPE,
-			BERRIES,
-			BlazeBurnerBlock.HEAT_LEVEL
 		);
 }
